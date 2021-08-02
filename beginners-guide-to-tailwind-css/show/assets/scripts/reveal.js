95af235d235006755277f62523b3395a0805789e
Vasanth Developer <vasanth@vasanthdeveloper.com>
2021-08-02 12:26:15 +0530

    ✨ only first slide is now centered

--- beginners-guide-to-tailwind-css/show/assets/scripts/reveal.js
+++ beginners-guide-to-tailwind-css/show/assets/scripts/reveal.js
@@ -2,6 +2,8 @@ import Reveal from 'https://cdn.skypack.dev/reveal.js'
 import Markdown from 'https://cdn.skypack.dev/reveal.js/plugin/markdown/markdown.esm.js'
 
 const deck = new Reveal({
+    center: false,
+    hash: true,
     progress: true,
     transition: 'convex',
     plugins: [
