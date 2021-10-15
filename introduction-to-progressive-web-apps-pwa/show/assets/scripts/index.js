e62920462b07f5a106fcd0f445d329701753bab2
Vasanth Developer <vsnthdev@gmail.com>
2021-10-15 21:16:47 +0530

    ✨ | created about me slide

--- introduction-to-progressive-web-apps-pwa/show/assets/scripts/index.js
+++ introduction-to-progressive-web-apps-pwa/show/assets/scripts/index.js
@@ -1,2 +1,12 @@
 import './twind.js'
 import './reveal.js'
+import axios from 'https://cdn.skypack.dev/axios'
+
+window.addEventListener('load', async () => {
+    const { data } = await axios({
+        method: 'GET',
+        url: 'https://api.vsnth.dev'
+    })
+
+    document.querySelector('#bio').textContent = data.bio
+})
