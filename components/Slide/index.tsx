18ceb60213813908a02c156c412950e5ec6fb785
Vasanth Srivatsa <vsnthdev@gmail.com>
2024-03-31 15:22:55 +0530

    🎊 optimize font sizes for mobile

--- components/Slide/index.tsx
+++ components/Slide/index.tsx
@@ -32,39 +32,39 @@ export function Slide({ slide }: { slide: Slide }) {
             </div>
         </DynamicallyLink>
 
-        <div className='py-6 px-4 flex flex-col space-y-4 md:rounded-xl md:bg-stone-800 md:border md:border-stone-700 md:-mt-14 md:ml-24 md:px-8 md:py-8 lg:px-10 lg:py-10'>
+        <div className='py-6 flex flex-col space-y-4 md:rounded-xl md:bg-stone-800 md:border md:border-stone-700 md:-mt-14 md:ml-24 md:px-8 md:py-8 lg:px-10 lg:py-10'>
             {/* event & session title */}
             <div className='flex flex-col space-y-1'>
-                {slide.for && <p className='text-lg font-medium opacity-70'>{slide.for}</p>}
-                {slide.title && <h3 className='font-medium text-2xl lg:text-3xl'>{slide.title}</h3>}
+                {slide.for && <p className='font-medium opacity-70 md:text-lg'>{slide.for}</p>}
+                {slide.title && <h3 className='font-medium text-xl md:text-2xl lg:text-3xl'>{slide.title}</h3>}
             </div>
 
             {/* description */}
-            {slide.description && <div className='flex flex-col text-lg font-content'>
+            {slide.description && <div className='flex flex-col font-content'>
                 <p>{slide.description}</p>
             </div>}
 
             {/* stats */}
             <div className='flex items-center justify-between max-w-md md:justify-start md:space-x-8'>
                 {slide.duration && <div className='flex items-center space-y-2 space-x-3'>
-                    <ClockIcon className='size-6' />
+                    <ClockIcon className='size-5 md:size-6' />
                     <div className='flex flex-col justify-center'>
-                        <span className='font-semibold uppercase text-base'>{slide.duration.split(' ')[0]}</span>
-                        <span className='text-sm font-semibold uppercase opacity-70'>{slide.duration.split(' ').slice(1).join(' ')}</span>
+                        <span className='text-sm md:text-base font-semibold uppercase'>{slide.duration.split(' ')[0]}</span>
+                        <span className='text-xs md:text-sm font-semibold uppercase opacity-70'>{slide.duration.split(' ').slice(1).join(' ')}</span>
                     </div>
                 </div>}
                 {slide.audianceSize && <div className='flex items-center space-y-2 space-x-3'>
-                    <UsersRoundIcon className='size-6' />
+                    <UsersRoundIcon className='size-5 md:size-6' />
                     <div className='flex flex-col justify-center'>
-                        <span className='font-semibold uppercase text-base'>{slide.audianceSize.split(' ')[0]}</span>
-                        <span className='text-sm font-semibold uppercase opacity-70'>{slide.audianceSize.split(' ').slice(1).join(' ')}</span>
+                        <span className='text-sm md:text-base font-semibold uppercase'>{slide.audianceSize.split(' ')[0]}</span>
+                        <span className='text-xs md:text-sm font-semibold uppercase opacity-70'>{slide.audianceSize.split(' ').slice(1).join(' ')}</span>
                     </div>
                 </div>}
                 {slide.sessionType && <div className='flex items-center justify-center space-y-2 space-x-3'>
-                    {slide.sessionType == 'Physical' ? <HandshakeIcon className='size-6' /> : <ClapperboardIcon className='size-6' />}
+                    {slide.sessionType == 'Physical' ? <HandshakeIcon className='size-5 md:size-6' /> : <ClapperboardIcon className='size-5 md:size-6' />}
                     <div className='flex flex-col justify-center'>
-                        <span className='font-semibold uppercase text-base'>{slide.sessionType}</span>
-                        <span className='text-sm font-semibold uppercase opacity-70'>session</span>
+                        <span className='text-sm md:text-base font-semibold uppercase'>{slide.sessionType}</span>
+                        <span className='text-xs md:text-sm font-semibold uppercase opacity-70'>session</span>
                     </div>
                 </div>}
             </div>
